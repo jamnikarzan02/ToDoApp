0562a8e5cfe1aff3bdabb3a14ce8d611c5fa9aa7
jamnikarzan02 <jamnikarzan02@gmail.com>
2026-04-10 15:36:41 +0200

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,34 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        TaskManager manager = new TaskManager();
+
+        while (true)
+        {
+            Console.WriteLine("1. Add task");
+            Console.WriteLine("2. Show tasks");
+            Console.WriteLine("3. Exit");
+
+            var input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    Console.Write("Enter task: ");
+                    string title = Console.ReadLine();
+                    manager.AddTask(title);
+                    break;
+
+                case "2":
+                    manager.ShowTasks();
+                    break;
+
+                case "3":
+                    return;
+            }
+        }
+    }
+}
