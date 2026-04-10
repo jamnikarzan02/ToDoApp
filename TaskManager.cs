d32844fb2df5f9bcf4ab5504c333b4a65db372cc
jamnikarzan02 <jamnikarzan02@gmail.com>
2026-04-10 15:40:11 +0200

    TaskManager.cs

---
+++ TaskManager.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+public class TaskManager
+{
+    private List<TaskItem> tasks = new List<TaskItem>();
+
+    public void AddTask(string title)
+    {
+        tasks.Add(new TaskItem(title));
+    }
+
+    public void ShowTasks()
+    {
+        for (int i = 0; i < tasks.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {tasks[i].Title}");
+        }
+    }
+}
